c4956b4cf73f280f1567403a26cbff5fcc10849f
Wii-Yang <wii_yang@163.com>
2026-04-01 12:00:12 +0800

    chore: 添加 @\@types 别名

--- vite.config.ts
+++ vite.config.ts
@@ -3,14 +3,27 @@ import vue from "@vitejs/plugin-vue";
 import AutoImport from "unplugin-auto-import/vite";
 import Components from "unplugin-vue-components/vite";
 import { ElementPlusResolver } from "unplugin-vue-components/resolvers";
+import path from "node:path";
 
 // https://vite.dev/config/
 export default defineConfig({
   base: "/image-coloring",
   plugins: [
     vue(),
-    AutoImport({ resolvers: ElementPlusResolver() }),
-    Components({ resolvers: ElementPlusResolver() }),
+    AutoImport({
+      dts: "types/auto-imports.d.ts",
+      resolvers: ElementPlusResolver(),
+    }),
+    Components({
+      dts: "types/components.d.ts",
+      resolvers: ElementPlusResolver(),
+    }),
   ],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "src"),
+      "@types": path.resolve(__dirname, "types"),
+    },
+  },
   server: { open: false, host: true, port: 80 },
 });
